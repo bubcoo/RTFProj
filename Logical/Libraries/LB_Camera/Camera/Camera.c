dfd8d0973d522591ee897fba8884982ff4d5e9f7
Ľubomír Bubeník <lubo.bubco@gmail.com>
2017-04-18 09:03:19 +0200

    added test function for testing ball speed and count of images

--- Logical/Libraries/LB_Camera/Camera/Camera.c
+++ Logical/Libraries/LB_Camera/Camera/Camera.c
@@ -6,29 +6,30 @@
 #endif
 
 #define NaN sqrt(-1)
-#define CAM_OFFLINE_MASK 0x10000
+#define CAM_OFFLINE_MASK 128												//1000 0000b
+#define CAM_ONLINE_MASK 0xFFFFFF7F											
 
-#define ABS_F(X) ((X) < (0) ? -(X) : (X))
-							//		  |						9800
-#define X1_REF_PLGND 800.0	//(UNITS) |			   3800 6800  | 11300
-#define X2_REF_PLGND 11300.0//(UNITS) |			 _____|___|___|_|_
-#define X1_REF_CAM 1215.0	//(PIXELS)|			| | | O O O O | | |		SCHEME OF X AXIS
-#define X2_REF_CAM 82.0		//(PIXELS)|			| | O | O O | O | |		AND PLAYGROUND WITH
- 							//		  |		    | O | O O O O | O |		DISTANCE OF PLAYERS
-#define Y1_REF_PLGND 820.0	//(UNITS) |	 	  	| | O | O O | O | |		IN UNITS
-#define Y2_REF_PLGND -950.0 //(UNITS) |			|_|_|_O_O_O_O_|_|_|		(10 UNITS = 1 MM)
-#define Y1_REF_CAM 414.0	//(PIXELS)| 		  | |   |   |
-#define Y2_REF_CAM 604.0	//(PIXELS)|		   800  | 5300 8300
-							//		  |		       2300
+#define ABS_F(X) ((X) < (0) ? -(X) : (X)) 									//ABS funkcion
+							//		  |							9800
+#define X1_REF_PLGND 800.0	//(UNITS) |				   3800 6800  | 11300
+#define X2_REF_PLGND 11300.0//(UNITS) |				 _____|___|___|_|_
+#define X1_REF_CAM 1215.0	//(PIXELS)|				| | | O O O O | | |		SCHEME OF X AXIS
+#define X2_REF_CAM 82.0		//(PIXELS)|				| | O | O O | O | |		AND PLAYGROUND WITH
+ 							//		  |		    	| O | O O O O | O |		DISTANCE OF PLAYERS
+#define Y1_REF_PLGND 820.0	//(UNITS) |	 	  		| | O | O O | O | |		IN UNITS
+#define Y2_REF_PLGND -950.0 //(UNITS) |				|_|_|_O_O_O_O_|_|_|		(10 UNITS = 1 MM)
+#define Y1_REF_CAM 414.0	//(PIXELS)| 		      | |   |   |
+#define Y2_REF_CAM 604.0	//(PIXELS)|			 	800 | 5300 8300
+							//		  |		    	   2300
 
-#define X_LIN_EQ_K ((X1_REF_PLGND-X2_REF_PLGND)/(X1_REF_CAM-X2_REF_CAM))
-#define X_LIN_EQ_Q (X1_REF_PLGND-(X1_REF_CAM*X_LIN_EQ_K))
+#define X_LIN_EQ_K ((X1_REF_PLGND-X2_REF_PLGND)/(X1_REF_CAM-X2_REF_CAM)) 		//Define K-parameter of line equaton for X axis
+#define X_LIN_EQ_Q (X1_REF_PLGND-(X1_REF_CAM*X_LIN_EQ_K))				 		//Define Q-parameter of line equaion for X axis
 
-#define Y_LIN_EQ_K ((Y1_REF_PLGND-Y2_REF_PLGND)/(Y1_REF_CAM-Y2_REF_CAM))
-#define Y_LIN_EQ_Q (Y1_REF_PLGND-(Y1_REF_CAM*Y_LIN_EQ_K))
+#define Y_LIN_EQ_K ((Y1_REF_PLGND-Y2_REF_PLGND)/(Y1_REF_CAM-Y2_REF_CAM))		//Define K-parameter of line equaton for Y axis
+#define Y_LIN_EQ_Q (Y1_REF_PLGND-(Y1_REF_CAM*Y_LIN_EQ_K))						//Define Q-parameter of line equaton for Y axis
 
-#define X_MAX_DIFF ABS_F((X_LIN_EQ_K+X_LIN_EQ_Q)-((2*X_LIN_EQ_K)+X_LIN_EQ_Q))
-#define Y_MAX_DIFF ABS_F((Y_LIN_EQ_K+Y_LIN_EQ_Q)-((2*Y_LIN_EQ_K)+Y_LIN_EQ_Q))
+#define X_MAX_DIFF ABS_F((X_LIN_EQ_K+X_LIN_EQ_Q)-((2*X_LIN_EQ_K)+X_LIN_EQ_Q))	//Line equation for X axis
+#define Y_MAX_DIFF ABS_F((Y_LIN_EQ_K+Y_LIN_EQ_Q)-((2*Y_LIN_EQ_K)+Y_LIN_EQ_Q))	//Line equation for Y axis
 
 void setError(struct FBCamera *inst,unsigned int status);
 void initFB(struct FBCamera *inst);
@@ -40,11 +41,8 @@ REAL RctToReal_ms (struct RTCtime_typ RTCtime);
 void FBCamera(struct FBCamera *inst)
 {	
 	if ((!inst->Enable) && (!inst->Internal.disableBit))
-		cleanFB(inst); 
-	
-	
-	getRealAxes(inst);
-	
+		cleanFB(inst);
+
 	switch (inst->Internal.MainSwitch){										//Main switch, DISABLED FB, INITialising FB, ENABLED FB and ERROR case
 		/******************************************************************/
 		/*							FB is DISABLED					 	  */
@@ -53,8 +51,6 @@ void FBCamera(struct FBCamera *inst)
 		case DISABLED:														//FB is disabled
 			if (inst->Internal.disableBit){
 				cleanFB(inst);
-				//TODO: settings after disable, do only once
-				//TODO: DevUnlink
 				inst->Internal.disableBit = 1;
 			}
 			if (inst->Enable){												//Wait for Enable = TRUE, else do nothing
@@ -62,7 +58,7 @@ void FBCamera(struct FBCamera *inst)
 				inst->Internal.disableBit = 0;	
 			}
 			else
-				inst->Status = ERR_FUB_ENABLE_FALSE;							//Status 65534, FB is called but not enabled
+				inst->Status = ERR_FUB_ENABLE_FALSE;						//Status 65534, FB is called but not enabled
 			break;
 		
 		/******************************************************************/
@@ -74,105 +70,44 @@ void FBCamera(struct FBCamera *inst)
 			if (!inst->InSight.ModuleOk){									//InSight Camera is not connected properly
 				inst->isCameraReady = 0;									//Bit for user that Camera is not working
 				if (inst->Search)											//If user allow searching for ball, change case to ERROR because you cannot find while there is no camera
-					setError(inst,ERR_CAM_NOTFOUND);						//Error, Status 65533 - camera not found
-				if (inst->MappView.LoadImage)
-					setError(inst,ERR_FTP_NOTREADY);	
+					setError(inst,ERR_CAM_NOTFOUND);						//Error, Status 65533 - camera not found	
 			}
 			else {
 				unsigned int status;
-					inst->isCameraReady = 1;									//Bit for user that Camera is working
-					inst->Internal.MainSwitch = RUN;							//Change state to RUN
-					inst->Status = ERR_OK;										//No error in enable case
-			}
-			break;
-	
-		case RUN:															//FB is enabled and camera communicating
-			if (!(inst->InSight.ModuleOk))									//if camera is disconnected change state to ENABLE where waiting for connect
-				inst->Internal.MainSwitch = ENABLED;
-			
-			if (inst->MappView.LoadImage){
-
-			}
-				
-			else if(inst->Search){
-				inst->isSearching = 1;										//FB is Searching ball
-				if (!inst->Internal.Search_tmp)								//Needed to change to camera trigger loop but only once
-				{
-					inst->Internal.CameraControl.ControlSwitch = TRIGGER_RDY;				//First state of camera triggering
-					inst->InSight.Control_I2000_S01 = ((inst->InSight.Control_I2000_S01 & 0xFFFFFF7F) | 1);				//Init condiotion in loop
-					inst->Internal.Search_tmp = 1;						// 		-||-
-				}
-			}	
-			else
-			{
-				inst->Internal.Search_tmp = inst->isSearching = 0;			//if not searching reset variables
-				inst->Internal.CameraControl.ControlSwitch = WAIT_CMD;
-			}
-			switch (inst->Internal.CameraControl.ControlSwitch){ 			//Camera trigger switch, process to get position of ball
-	
-				case WAIT_CMD:												//Searching for ball is stoped	
-					inst->InSight.Control_I2000_S01 = inst->InSight.Control_I2000_S01 & 0xFFFFFFF0;				//control bit is set to 0 for no action
-					break;
-			
-				case TRIGGER_RDY:
-					if ((inst->InSight.Status_I2001_S01 & 1) == 1){				//waiting for trigger status ready
-					
-						inst->InSight.Control_I2000_S01 =((inst->InSight.Control_I2000_S01 & 0xFFFFFFF3) | 3);				//#11 - enabled trigger and start trigger function 
-						inst->InSight.Status_I2001_S01 = 0;
-						inst->Internal.CameraControl.ControlSwitch = TRIGGER;
-					}	
-					break;
-						
-				case TRIGGER:
-					if ((inst->InSight.Status_I2001_S01 & 3) == 3){
-						inst->InSight.Control_I2000_S01 =((inst->InSight.Control_I2000_S01 & 0xFFFFFFF1) | 1);
-						//	inst->InSight.Control_I2000_S01 = inst->InSight.Control_I2000_S01 | 7;
-						inst->InSight.Status_I2001_S01 = 0;
-						inst->Internal.CameraControl.ControlSwitch = ACQ;
-					}
-					break;
-						
-				case ACQ:
-					if ((inst->InSight.Status_I2001_S01 & 8) == 8){
-						//TODO: missed acquirision
-						inst->Internal.CameraControl.ControlSwitch = TRIGGER_RDY;
-					}
-					if ((inst->InSight.Status_I2001_S01 & 4) != 4){
-					
-						inst->Internal.CameraControl.ControlSwitch = INSP;
-						/*	if ((inst->InSight.Status_I2001_S01 & 15) == 7)
-								inst->isBallFound = 1;
-							else
-								inst->isBallFound = 0;
-							
-							inst->InSight.Control_I2000_S01 = ((inst->InSight.Control_I2000_S01 | 9) & 0xFFFFFFF9);
-							inst->InSight.Status_I2001_S01 = 0;
-							inst->Internal.CameraControl.ControlSwitch = TRIGGER;*/
-					}
-					break;
-				
-				case INSP:
-					if ((inst->InSight.Status_I2001_S01 & 256) != 256){// && ((inst->InSight.Status_I2001_S01 & 512) == 512))
-						inst->isBallFound = 1;
-						inst->Internal.CameraControl.ControlSwitch = TRIGGER_RDY;
-					}
-					break;
+				inst->isCameraReady = 1;								//Bit for user that Camera is working
+				inst->Internal.MainSwitch = RUN;						//Change state to RUN
+				inst->Status = ERR_OK;									//No error in enable case
 			}
 			break;
 		
-		case ERROR:														//error case, to change state is needed error Reset variable or disable FB
-			inst->Error = 1;											//Change Error variable
-			inst->Internal.CameraControl.ControlSwitch = WAIT_CMD;		//Reset camera trigger state
-			inst->isSearching = 0;										//FB cannot searching in error case
-			if (inst->ErrorReset)										//waiting on ErrorReset
-			{	
+		case RUN:															//FB is enabled and camera communicating
+			if (!(inst->InSight.ModuleOk))									//if camera is disconnected change state to ENABLE where waiting for connect
+				inst->Internal.MainSwitch = ENABLED;
+			if (inst->Search){
+				inst->InSight.Control_I2000_S01 &= CAM_ONLINE_MASK;			//Disable control bit for cammera offline mode
+				inst->isSearching = 1;
+				if (inst->InSight.InspectionResults_I2011_S03 != inst->Internal.SuccesCount)
+					inst->isBallFound = 1;
+				else inst->isBallFound = 0;
+				getRealAxes(inst);
+			}
+			else{
+				inst->InSight.Control_I2000_S01 |= CAM_OFFLINE_MASK;
+				inst->isSearching = 0;
+			}
+			break;
+		
+		case ERROR:															//error case, to change state is needed error Reset variable or disable FB
+			inst->Error = 1;												//Change Error variable
+			inst->Internal.CameraControl.ControlSwitch = WAIT_CMD;			//Reset camera trigger state
+			inst->isSearching = 0;											//FB cannot searching in error case
+			if (inst->ErrorReset){											//waiting on ErrorReset
 				inst->Error = 0;
 				inst->Status = ERR_OK;
 				resetFB(inst);
-				inst->Internal.MainSwitch = ENABLED;					//change state
+				inst->Internal.MainSwitch = ENABLED;						//change state
 			}
 			break;
-
 	}
 		
 	if (inst->ErrorReset)										
@@ -180,6 +115,7 @@ void FBCamera(struct FBCamera *inst)
 		
 	if (inst->Error == 1)						
 		inst->Internal.MainSwitch = ERROR;
+	
 }
 
 void initFB(struct FBCamera *inst)
@@ -196,7 +132,7 @@ void initFB(struct FBCamera *inst)
 	inst->Internal.MainSwitch = DISABLED;
 	inst->Internal.Search_tmp = 0;
 	inst->Internal.disableBit = 1;
-	inst->InSight.Control_I2000_S01 = 128;
+	inst->InSight.Control_I2000_S01 |= CAM_OFFLINE_MASK;					
 	memset(&inst->Results.ActTime,0,sizeof(inst->Results.ActTime));
 	inst->Results.AxisXOld = NaN;
 	inst->Results.AxisYOld = NaN;
@@ -234,12 +170,12 @@ void getRealAxes(struct FBCamera *inst)
 			inst->Results.AxisYOld = inst->Results.AxisY;
 			inst->Results.AxisX = ((inst->InSight.InspectionResults_I2011_S01*(X_LIN_EQ_K))+X_LIN_EQ_Q);
 			inst->Results.AxisY = ((inst->InSight.InspectionResults_I2011_S02*(Y_LIN_EQ_K))+Y_LIN_EQ_Q);
+			
 		}
 		memcpy((UDINT)&inst->Results.ActTimeOld,(UDINT)&inst->Results.ActTime,sizeof(inst->Results.ActTime));
 		RTC_gettime(&inst->Results.ActTime);
 		inst->Results.TimeDiff_ms = ((RctToReal_ms(inst->Results.ActTime)) - (RctToReal_ms(inst->Results.ActTimeOld)));
-
-		
+		inst->isBallFound = 1;
 		inst->Internal.SuccesCount = inst->InSight.InspectionResults_I2011_S03;
 	}
 	else if ((inst->Internal.FailCount != inst->InSight.InspectionResults_I2011_S04) && ((!isnan(inst->Results.AxisXOld)) && (!isnan(inst->Results.AxisYOld)))){ 
@@ -247,6 +183,7 @@ void getRealAxes(struct FBCamera *inst)
 		inst->Results.AxisY = inst->Results.AxisYOld;
 		inst->Results.AxisXOld = inst->Results.AxisYOld = NaN;
 		inst->Internal.FailCount = inst->InSight.InspectionResults_I2011_S04;
+		inst->isBallFound = 0;
 	}
 }
 
